1f21f22c6d6fd11106bace1487c4012b88d4ee22
zero4994 <cavc94@gmail.com>
2019-02-11 17:03:56 +0900

    Removed default component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hello world</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
