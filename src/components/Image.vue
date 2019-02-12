9bb313472f3958e72ce2eb26c2258117b10ffa31
zero4994 <cavc94@gmail.com>
2019-02-12 11:46:39 +0900

    Implements separation of queries into a single file and adds Image component

---
+++ src/components/Image.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card m-3">
+    <div class="card-header">
+      <h5>{{image.Name}}</h5>
+    </div>
+    <div class="card-body">
+      <ul class="list-group mb-3">
+        <li class="list-group-item">
+          <p>Id: {{image.Id}}</p>
+        </li>
+        <li class="list-group-item">
+          <p>Created: {{image.Created}}</p>
+        </li>
+      </ul>
+      <button class="btn btn-success btn-block" v-on:click="changeView">RUN</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["image"],
+  data: function() {
+    return {
+      currentView: "singleImage"
+    };
+  },
+  mounted() {
+    console.log(this.currentView);
+  },
+  methods: {
+    changeView: function() {
+      this.currentView = "propsImage";
+    }
+  }
+};
+</script>
+
+
+<style>
+</style>
---
+++ src/queries/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  allImages: `{ 
+                Images {
+                    Id
+                    Name
+                    Created
+                }
+            }`
+};
