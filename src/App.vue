df8ef296dfce74ae424201f9ad45872c18529d0f
zero4994 <cavc94@gmail.com>
2019-02-12 12:02:16 +0900

    Added simple conditional rendering for images and containers component

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
   <div id="app">
     <navbar/>
     <div class="row container-fluid">
-      <div class="col-2 col-md-2">
-        <sidebar/>
+      <div class="col-sm-2">
+        <sidebar v-on:changeView="changeView"/>
       </div>
-      <div class="col-8 col-md-8">
-        <images-list/>
+      <div class="col-sm-10">
+        <images-list v-if="this.currentView === 'Images'"/>
+        <containers-list v-else/>
       </div>
     </div>
   </div>
@@ -16,13 +17,23 @@
 import Navbar from "./components/Navbar.vue";
 import Sidebar from "./components/Sidebar.vue";
 import ImagesList from "./components/ImagesList.vue";
+import ContainerList from "./components/ContainerList.vue";
 
 export default {
   name: "app",
   components: {
     navbar: Navbar,
     sidebar: Sidebar,
-    "images-list": ImagesList
+    "images-list": ImagesList,
+    "containers-list": ContainerList
+  },
+  data: () => ({
+    currentView: "Images"
+  }),
+  methods: {
+    changeView: function(value) {
+      this.currentView = value;
+    }
   }
 };
 </script>
@@ -33,5 +44,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+
+  /* background-color: #384d54;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 80 80' width='80' height='80'%3E%3Cpath fill='%230db7ed' fill-opacity='0.4' d='M14 16H9v-2h5V9.87a4 4 0 1 1 2 0V14h5v2h-5v15.95A10 10 0 0 0 23.66 27l-3.46-2 8.2-2.2-2.9 5a12 12 0 0 1-21 0l-2.89-5 8.2 2.2-3.47 2A10 10 0 0 0 14 31.95V16zm40 40h-5v-2h5v-4.13a4 4 0 1 1 2 0V54h5v2h-5v15.95A10 10 0 0 0 63.66 67l-3.47-2 8.2-2.2-2.88 5a12 12 0 0 1-21.02 0l-2.88-5 8.2 2.2-3.47 2A10 10 0 0 0 54 71.95V56zm-39 6a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm40-40a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM15 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm40 40a2 2 0 1 0 0-4 2 2 0 0 0 0 4z'%3E%3C/path%3E%3C/svg%3E"); */
 }
 </style>
