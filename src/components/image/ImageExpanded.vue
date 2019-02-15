4cc2efdb5b3fe0f23d36fd2dc94094a9be1efc1a
zero4994 <cavc94@gmail.com>
2019-02-16 02:12:05 +0900

    Adds styling for the expanded image

--- src/components/image/ImageExpanded.vue
+++ src/components/image/ImageExpanded.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <!-- <div class="container">
     <h2>{{this.image.Name}}</h2>
     <div class>
       <div class="card">
@@ -25,14 +25,46 @@
         <i class="pl-2 fas fa-play"></i>
       </button>
     </div>
+  </div>-->
+  <div>
+    <v-container fluid grid-list-md>
+      <v-layout row wrap>
+        <h2>{{this.image.RepoTags}}</h2>
+      </v-layout>
+    </v-container>
+
+    <v-container fluid grid-list-md>
+      <v-layout row wrap>
+        <v-flex v-bind="{ [`md8`]: true }">
+          <v-card>
+            <v-card-actions>
+              <span class="headline">Container</span>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+        <v-flex v-bind="{ [`md4`]: true }">
+          <v-card>
+            <v-card-actions>
+              <span class="headline">Additional Info</span>
+            </v-card-actions>
+            <image-additional-info v-bind:image="image"/>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 
 <script>
 import axios from "axios";
+import ImageAdditionalInfo from "./ImageAdditionalInfo.vue";
+
 export default {
   props: ["image"],
+  components: {
+    "image-additional-info": ImageAdditionalInfo
+  },
   data: () => ({
     hostPort: "",
     localPort: ""
