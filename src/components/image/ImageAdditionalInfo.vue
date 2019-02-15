8582488a1d9c061b8beec782891660d4de8c7004
zero4994 <cavc94@gmail.com>
2019-02-16 02:11:47 +0900

    Adds component to display the additional information about the image

---
+++ src/components/image/ImageAdditionalInfo.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-list>
+    <v-list-tile>
+      <v-list-tile-content>
+        <v-list-tile-title>ID</v-list-tile-title>
+        <v-list-tile-sub-title>{{this.image.Id}}</v-list-tile-sub-title>
+      </v-list-tile-content>
+    </v-list-tile>
+    <v-list-tile>
+      <v-list-tile-content>
+        <v-list-tile-title>Created</v-list-tile-title>
+        <v-list-tile-sub-title>{{this.image.Created}}</v-list-tile-sub-title>
+      </v-list-tile-content>
+    </v-list-tile>
+    <v-list-tile>
+      <v-list-tile-content>
+        <v-list-tile-title>Labels</v-list-tile-title>
+        <v-list-tile-sub-title>{{this.image.Label}}</v-list-tile-sub-title>
+      </v-list-tile-content>
+    </v-list-tile>
+    <v-list-tile>
+      <v-list-tile-content>
+        <v-list-tile-title>ParentId</v-list-tile-title>
+        <v-list-tile-sub-title>{{this.image.ParentId}}</v-list-tile-sub-title>
+      </v-list-tile-content>
+    </v-list-tile>
+    <v-list-tile>
+      <v-list-tile-content>
+        <v-list-tile-title>RepoDigests</v-list-tile-title>
+        <v-list-tile-sub-title>{{this.image.RepoDigests}}</v-list-tile-sub-title>
+      </v-list-tile-content>
+    </v-list-tile>
+    <v-list-tile>
+      <v-list-tile-content>
+        <v-list-tile-title>SharedSize</v-list-tile-title>
+        <v-list-tile-sub-title>{{this.image.SharedSize}}</v-list-tile-sub-title>
+      </v-list-tile-content>
+    </v-list-tile>
+    <v-list-tile>
+      <v-list-tile-content>
+        <v-list-tile-title>Size</v-list-tile-title>
+        <v-list-tile-sub-title>{{ this.bytesToSize(this.image.Size) }}</v-list-tile-sub-title>
+      </v-list-tile-content>
+    </v-list-tile>
+    <v-list-tile>
+      <v-list-tile-content>
+        <v-list-tile-title>VirtualSize</v-list-tile-title>
+        <v-list-tile-sub-title>{{ this.bytesToSize(this.image.VirtualSize) }}</v-list-tile-sub-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
+    <!-- <v-list-tile>
+      <v-list-tile-content>
+        <v-list-tile-title>RepoTags</v-list-tile-title>
+        <v-list-tile-sub-title>
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-content>
+                <v-list-tile-title>ID</v-list-tile-title>
+                <v-list-tile-sub-title>{{this.image.Id}}</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-list-tile-sub-title>
+      </v-list-tile-content>
+    </v-list-tile>-->
+  </v-list>
+</template>
+
+<script>
+export default {
+  props: ["image"],
+  methods: {
+    bytesToSize: function(bytes) {
+      const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+      if (bytes == 0) return "0 Byte";
+      const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+      return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
+    }
+  }
+};
+</script>
+
+<style>
+</style>
