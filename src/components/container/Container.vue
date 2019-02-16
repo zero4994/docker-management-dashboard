9ccc2271d23ee086709bd6ad4a72608c0f900564
zero4994 <cavc94@gmail.com>
2019-02-16 16:35:37 +0900

    Adds container component to show all details of the container

---
+++ src/components/container/Container.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello World {{this.container.Id}}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ["container"]
+};
+</script>
+
+
+<style>
+</style>
