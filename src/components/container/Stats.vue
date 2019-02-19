3e070bcd33bf2ddab6e07a1647e4d9af665f8efe
zero4994 <cavc94@gmail.com>
2019-02-20 01:15:02 +0900

    Added component for stats

---
+++ src/components/container/Stats.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card>
+    <v-card-actions>
+      <span class="headline">Stats</span>
+    </v-card-actions>
+  </v-card>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+
+<style>
+</style>
