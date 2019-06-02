3db0f5f6c7e4615e13b9378c9c15696a37d5c9d8
zero4994 <cavc94@gmail.com>
2019-06-02 23:46:37 +0900

    Removed unneeded dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
 import App from "./App.vue";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "vuetify/dist/vuetify.min.css";
 
 Vue.config.productionTip = false;
