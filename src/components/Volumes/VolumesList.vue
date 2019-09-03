37b9104a2e0d3296beeffdb8a4bf3b439db96485
zero4994 <cavc94@gmail.com>
2019-09-03 11:36:26 -0500

    Implements volume service

--- src/components/Volumes/VolumesList.vue
+++ src/components/Volumes/VolumesList.vue
@@ -1,9 +1,44 @@
 <template>
-  <h1>Volumes</h1>
+  <div>
+    <div v-if="this.currentView === 'all'">
+      <v-container fluid grid-list-md>
+        <v-layout row wrap>
+          <h1>Volumes</h1>
+          <v-spacer></v-spacer>
+          <v-btn color="cyan" dark @click="fetchAllVolumes">
+            Refresh
+            <v-icon>refresh</v-icon>
+          </v-btn>
+        </v-layout>
+      </v-container>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { allVolumes } from "../../services/VolumeService";
+export default {
+  data: () => ({
+    currentView: "all",
+    volumes: []
+  }),
+  methods: {
+    fetchAllVolumes: async function() {
+      try {
+        const result = await allVolumes();
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+        this.$dialog.message.error("Error listing volumes", {
+          position: "top-left"
+        });
+      }
+    }
+  },
+  mounted() {
+    this.fetchAllVolumes();
+  }
+};
 </script>
 
 <style>
