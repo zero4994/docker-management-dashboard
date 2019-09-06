c93c9442c84554cbd5f6fb96c4ac3847871c1cb8
zero4994 <cavc94@gmail.com>
2019-09-05 21:11:40 -0500

    Adds algorithm to group images by name

--- src/services/ImageService.js
+++ src/services/ImageService.js
@@ -3,6 +3,43 @@ import { docker } from "./config";
 export const allImages = async function() {
   console.log("Querying all images...");
   const images = await docker.listImages({ all: false });
-  console.log("Images: ", images);
-  return images;
+  return groupImagesByTag(images);
+};
+
+const groupImagesByTag = function(images) {
+  /*
+    Should group images by name and have the tags available
+    [ { name: "ubuntu", 
+        tags: { 
+          "19.10": { containers, id, created }, 
+          "18.04": { containers, id, created } }
+    } ]
+  */
+  const imagesArray = [];
+  const cache = {};
+
+  images.forEach(element => {
+    const name = element.RepoTags[0].split(":")[0];
+
+    if (!cache[name]) {
+      const image = {
+        name,
+        tags: {}
+      };
+
+      let currentElement;
+      for (let i = 0; i < images.length; i++) {
+        currentElement = images[i];
+        const [currentName, currentTag] = currentElement.RepoTags[0].split(":");
+        if (currentName === name) {
+          image.tags[currentTag] = currentElement;
+        }
+      }
+      cache[name] = 1;
+      imagesArray.push(image);
+    }
+  });
+
+  console.log("===>", imagesArray);
+  return imagesArray;
 };
