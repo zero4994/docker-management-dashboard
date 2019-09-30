e86059a74b0232a4ec08160aa6cec94cc22b6e2f
zero4994 <cavc94@gmail.com>
2019-09-30 18:31:35 +0900

    Adds labels corresponding the status of containers and interval every 5 second to update them

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,33 @@
 <template>
   <v-footer class="pa-3" style="min-height: 0px" dark color="light-blue darken-2">
     <div class="mr-4">Images: 30</div>
-    <div>Running Containers: 2</div>
+    <div class="mr-4">Running Containers: {{ this.status.running }}</div>
+    <div class="mr-4">Paused Containers: {{ this.status.paused }}</div>
+    <div>Exited Containers: {{ this.status.exited }}</div>
   </v-footer>
 </template>
 
 <script>
-export default {};
+import { getContainersByStatus } from "../services/ContainerService";
+
+export default {
+  mounted() {
+    this.updateFooter();
+    setInterval(this.updateFooter, 5000);
+  },
+  data: () => ({
+    status: {
+      running: 0,
+      paused: 0,
+      exited: 0
+    }
+  }),
+  methods: {
+    updateFooter: async function() {
+      this.status = await getContainersByStatus();
+    }
+  }
+};
 </script>
 
 <style>
