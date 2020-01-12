005fbfadc24311b9a5b23bddfce413b4975e7d43
zero4994 <cavc94@gmail.com>
2020-01-13 00:59:42 +0900

    chore: adds component to display containers in table

---
+++ src/components/container/ContainerTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { allContainers } from "../../services/ContainerService.js";
+
+export default {
+  data: () => ({
+    containers: [],
+    interval: {}
+  }),
+  mounted: function() {
+    this.fetchAllContainers();
+    this.interval = setInterval(() => {
+      this.fetchAllContainers();
+    }, 5000);
+  },
+  methods: {
+    fetchAllContainers: async function() {
+      this.containers = await allContainers.bind(this)();
+    }
+  },
+  beforeDestroy() {
+    console.log("clearing interval");
+    clearInterval(this.interval);
+  }
+};
+</script>
+
+<style></style>
