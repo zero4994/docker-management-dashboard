0af4470163c4fbe3dbabd862fdae68d81f6a74e7
zero4994 <cavc94@gmail.com>
2020-01-13 01:12:05 +0900

    chore: adds table and displays containers

--- src/components/container/ContainerTable.vue
+++ src/components/container/ContainerTable.vue
@@ -1,5 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="this.containers"
+      class="elevation-1 mt-4 mb-4"
+      :rows-per-page-items="[10]"
+    >
+      <template v-slot:items="props">
+        <td>{{ props.item.Image }}</td>
+        <td>{{ props.item.Id.substr(0, 15) }}</td>
+        <td>{{ props.item.Created }}</td>
+        <td>{{ props.item.Status }}</td>
+        <td>{{ props.item.State }}</td>
+        <v-btn
+          color="red darken-3"
+          :dark="!isDisabled"
+          @click="onStopContainer"
+          :disabled="isDisabled"
+        >
+          <v-icon>stop</v-icon>
+        </v-btn>
+        <v-btn
+          color="light-green lighten-1"
+          :dark="!isDisabled"
+          @click="onUnpauseContainer"
+          :disabled="isDisabled"
+        >
+          <v-icon>play_arrow</v-icon>
+        </v-btn>
+        <v-btn
+          color="grey darken-4"
+          :dark="!isDisabled"
+          @click="onRemoveContainer"
+          :disabled="isDisabled"
+        >
+          <i class="fas fa-trash-alt"></i>
+        </v-btn>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
@@ -8,7 +47,39 @@ import { allContainers } from "../../services/ContainerService.js";
 export default {
   data: () => ({
     containers: [],
-    interval: {}
+    interval: {},
+    headers: [
+      {
+        text: "Name",
+        align: "left",
+        sortable: false,
+        value: "Image"
+      },
+      {
+        text: "ID",
+        align: "left",
+        sortable: false,
+        value: "Id"
+      },
+      {
+        text: "Created",
+        align: "left",
+        sortable: false,
+        value: "Created"
+      },
+      {
+        text: "Status",
+        align: "left",
+        sortable: false,
+        value: "Status"
+      },
+      {
+        text: "State",
+        align: "left",
+        sortable: false,
+        value: "State"
+      }
+    ]
   }),
   mounted: function() {
     this.fetchAllContainers();
@@ -19,6 +90,7 @@ export default {
   methods: {
     fetchAllContainers: async function() {
       this.containers = await allContainers.bind(this)();
+      console.log({ containers: this.containers });
     }
   },
   beforeDestroy() {
